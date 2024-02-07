5eab0531e9fd87f25af444fc80a64bb75b01a925
LaetitiaMantel <laetitia.lecroart@gmail.com>
2024-02-07 13:25:47 +0100

    delete entité customer

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,16 +2,23 @@
 
 namespace App\DataFixtures;
 
+use Faker;
+use App\Entity\Brand;
+use App\Entity\Product;
+use App\DataFixtures\Provider\OfajitasProvider;
+use DateTimeImmutable;
+use DateTimeZone;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+
 
 class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
-
-        $manager->flush();
+        
+             // $manager->persist($product);
+        
+            // $manager->flush();
     }
 }
