149fff965ded657d4b1f144ee66c4ef77c5e3692
annatariachvili <anna.tariachvili@icloud.com>
2024-02-08 09:42:11 +0100

    update brand files

--- src/Controller/BrandController.php
+++ src/Controller/BrandController.php
@@ -9,25 +9,24 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 
-  class BrandController extends AbstractController
+class BrandController extends AbstractController
 {
     #[Route('/brand/{slug}', name: 'front_brand_show')]
     public function brandProducts(string $slug, ProductRepository $productRepository, BrandRepository $brandRepository): Response
     {
-        // récupère les produits par Marques 
-        $brands  = $brandRepository->findAll();
-        $products = $productRepository->findByBrand($slug);
-        
+        // Récupère la marque correspondant au slug
+        $selectedBrand = $brandRepository->findOneBy(['slug' => $slug]);
 
-        
+        // Récupère les produits par marque
+        $products = $productRepository->findByBrand($slug);
+
         return $this->render('brand/index.html.twig', [
             'controller_name' => 'BrandController',
-            'brand' => $slug,
-            'brands' => $brands, 
+            'selectedBrand' => $selectedBrand,
             'products' => $products,
-
         ]);
     }
 }
+
 
 
