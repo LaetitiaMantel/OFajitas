6e6462238965810a5609d4ef379b3149a84a1e75
LaetitiaMantel <laetitia.lecroart@gmail.com>
2024-02-08 16:14:27 +0100

    bug searchbar

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -7,6 +7,7 @@ use App\Repository\ProductRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 
 #[Route('/', name: 'front_main_')]
 class MainController extends AbstractController
@@ -24,4 +25,18 @@ class MainController extends AbstractController
             'categoriesByOrder'   => $categoriesByOrder
         ]);
     }
+
+        // route pour la barre de recherche
+        #[Route('/search', name: 'search')]
+        public function research(Request $request, ProductRepository $productRepository)
+        {
+            $search = $request->query->get('search');
+            if ($search){
+            $products = $productRepository->findByResearch($search);
+            }
+            return $this->render('search.html.twig', [
+                'products' => $products,
+                'search' => $search,
+            ]);
+        }
 }
--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use App\Repository\BrandRepository;
 use App\Repository\ProductRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -12,7 +13,8 @@ class ProductController extends AbstractController
 {
     // route pour afficher tous les produits
     #[Route('/product', name: 'front_product_index')]
-    public function index(ProductRepository $productRepository, BrandRepository $brandRepository): Response
+
+    public function index(ProductRepository $productRepository,): Response
     {
         $products = $productRepository->findAll();
 
@@ -21,7 +23,10 @@ class ProductController extends AbstractController
         ]);
     }
 
-    // route pour afficher les details d'un produits
+
+
+
+    // route pour afficher les details d'un produit
     #[Route('/product/{slug}', name: 'front_product_show')]
     public function show(string $slug, ProductRepository $productRepository): Response
     {
