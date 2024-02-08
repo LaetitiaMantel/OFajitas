462717109e863f25b52b7fd74d6555f558000c04
franckfajitas <franck.richard@oclock.school>
2024-02-08 10:07:01 +0100

    Category Controller

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -8,16 +8,18 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
+
 class CategoryController extends AbstractController
 {
     #[Route('/categorie/{slug}', name: 'front_categories_show')]
     public function ProductsCategory(string $slug, CategoryRepository $categoryRepository, ProductRepository $productRepository): Response
     {
         $products = $productRepository->findByCategory($slug);
-dd($slug);
-        return $this->render('categories/index.html.twig', [
+
+        return $this->render('category/index.html.twig', [
             'controller_name' => 'CategoryController',
             'products' => $products,
+            'categorie' => $slug,
     ]);
     }
 }
