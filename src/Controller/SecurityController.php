c9f55254d6d7849fc98c96c301b0dbe1f8953130
jonathan-53 <jonathanaulnette@gmail.com>
2024-02-14 16:42:54 +0100

    create mail for inscription

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -2,10 +2,14 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+use Symfony\Component\Security\Core\Authentication\Token\UsernamePasswordToken;
 
 class SecurityController extends AbstractController
 {
@@ -29,4 +33,76 @@ class SecurityController extends AbstractController
     {
         throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
     }
+
+    // #[Route(path: '/autologin/{email}/{password}', name: 'autologin')]
+    // public function autoLogin(EntityManagerInterface $entityManager, string $email, string $password): Response
+    // {
+    //     // Recherche de l'utilisateur dans la base de données en fonction de l'email
+    //     $userRepository = $entityManager->getRepository(User::class);
+    //     $user = $userRepository->findOneBy(['email' => $email]);
+
+    //     // Vérification si l'utilisateur existe et si le mot de passe est correct
+    //     if ($user && password_verify($password, $user->getPassword())) {
+    //         // Connexion de l'utilisateur (remplacez cette logique par la manière dont vous gérez les connexions dans votre application)
+    //         // Exemple: vous pourriez utiliser le composant Security de Symfony pour gérer l'authentification
+    //         // Veuillez ajuster cette partie en fonction de votre système d'authentification
+    //         // Cette partie suppose que vous utilisez Symfony Security et que vous avez configuré un firewall nommé 'main'
+           
+    //         // Redirection de l'utilisateur vers une page après connexion réussie
+    //         return $this->redirectToRoute('front_main_home');
+    //     }
+
+    //     // Si les informations d'identification sont incorrectes, redirigez l'utilisateur vers une page d'erreur ou affichez un message d'erreur
+    //     return $this->redirectToRoute('front_main_new');
+    // }
+
+
+    // #[Route(path: '/autologin/{email}/{password}', name: 'autologin')]
+    // public function autoLogin(User $user, EntityManagerInterface $entityManager, string $email, string $password):Response
+    // {
+  
+    // // Recherche de l'utilisateur dans la base de données en fonction de l'email
+    // $userRepository = $entityManager->getRepository(User::class);
+    // $user = $userRepository->findOneBy(['email' => $email]);
+    // // Vérification si l'utilisateur existe et si le mot de passe est correct
+    // if ('password' == $user->getPassword()) {
+        
+    //     // Redirection de l'utilisateur vers une page après connexion réussie
+    //     //return $this->redirectToRoute('front_main_home');
+        
+    //     dump($user);
+    // }
+    // dump($user);
+    
+    // }   
+
+//     #[Route(path: '/autologin/{email}/{password}', name: 'autologin')]
+// public function autoLogin(User $user, EntityManagerInterface $entityManager, string $email, string $password):Response
+// {
+
+// // Recherche de l'utilisateur dans la base de données en fonction de l'email
+// $userRepository = $entityManager->getRepository(User::class);
+// $user = $userRepository->findOneBy(['email' => $email]);
+
+// // Vérification si l'utilisateur existe et si le mot de passe est correct
+// if (!$user || !password_verify($password, 'jo')) {
+// $this->addFlash('error', 'Identifiants invalides.');
+// //dd($user);
+// return $this->redirectToRoute('login');
+// }
+
+// // Création d'un jeton d'authentification
+// //$providerKey = 'secured_area'; // Remplacez par la clé de votre fournisseur
+// //token = new UsernamePasswordToken($user, null, $providerKey, $user->getRoles());
+
+// // Stockage du jeton dans la mémoire de sécurité
+// //$this->container->get('security.token_storage')->setToken($token);
+
+// // Redirection vers la page d'accueil
+// return $this->redirectToRoute('front_main_home');
+
+// }
+
+
+   
 }
