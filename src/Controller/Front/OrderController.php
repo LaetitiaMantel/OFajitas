c8ac8853b4a638257f43ba417cf0160915ba78b1
annatariachvili <anna.tariachvili@icloud.com>
2024-02-15 23:45:02 +0100

    command fake payment

--- src/Controller/Front/OrderController.php
+++ src/Controller/Front/OrderController.php
@@ -4,19 +4,18 @@ namespace App\Controller\Front;
 
 use App\Entity\LigneOrder;
 use App\Entity\Order;
-use App\Repository\ProductRepository;
 use App\Service\CartManager;
+use App\Service\FakePaymentService; // Ajout du service fictif de paiement
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Component\Routing\Annotation\Route;
 
 #[Route('/commandes', name: 'front_order_')]
 class OrderController extends AbstractController
 {
     #[Route('/valider', name: 'add', methods: ['GET', 'POST'])]
-    public function add(CartManager $cartManager, EntityManagerInterface $em): Response
+    public function add(CartManager $cartManager, EntityManagerInterface $em, FakePaymentService $fakePaymentService): Response
     {
         $this->denyAccessUnlessGranted('ROLE_USER');
 
@@ -25,7 +24,6 @@ class OrderController extends AbstractController
             return $this->redirectToRoute('front_main_new');
         }
 
-        // Récupérez le panier à partir du CartManager
         $cart = $cartManager->getCart();
 
         if (empty($cart)) {
@@ -33,37 +31,45 @@ class OrderController extends AbstractController
             return $this->redirectToRoute('front_main_home');
         }
 
-        // Créez la commande
         $order = new Order();
         $order->setUser($this->getUser());
         $order->setRef(uniqid());
         $order->setCreatedAt(new \DateTimeImmutable());
 
-        // Persistez la commande principale
         $em->persist($order);
 
-        // Parcourez le panier pour créer les détails de commande
+        $totalAmount = 0;
+
         foreach ($cart as $cartItem) {
             $ligneOrder = new LigneOrder();
             $product = $cartItem['product'];
             $quantity = $cartItem['quantity'];
 
-            // Configurez les détails de commande
             $ligneOrder->setName($product->getName());
             $ligneOrder->setPrice($product->getPrice());
             $ligneOrder->setQuantity($quantity);
             $ligneOrder->setCreatedAt(new \DateTimeImmutable());
 
-            // Ajoutez les détails de commande à la commande principale
             $order->addLigneOrder($ligneOrder);
 
-            // Ne pas persister le produit ici, car il a déjà été persisté dans la base de données
-            // Ne pas utiliser $em->persist($product);
+            $totalAmount += $product->getPrice() * $quantity;
         }
 
         // Flush uniquement la commande principale, les LigneOrder sont persistées automatiquement grâce à l'annotation "cascade" dans l'entité Order
         $em->flush();
 
-        return $this->redirectToRoute('front_main_home'); // Redirigez après avoir validé la commande
+        // Utilisez l'ID après l'appel à $em->flush()
+        $paymentResult = $fakePaymentService->processPayment($order->getId(), $totalAmount);
+
+        if ($paymentResult) {
+            return $this->render('front/order/add.html.twig', [
+                'cart' => $cart,
+                'totalAmount' => $totalAmount,
+                'paymentResult' => true,
+            ]);
+        } else {
+            $this->addFlash('error', 'Le paiement a échoué. Veuillez réessayer.');
+            return $this->redirectToRoute('front_main_home');
+        }
     }
 }
