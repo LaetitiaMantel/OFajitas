5ea1ed0f7c0df2909a4ab447feb6b3c9a486e05e
annatariachvili <anna.tariachvili@icloud.com>
2024-02-19 09:52:58 +0100

    merge dev and order no conflict

--- migrations/Version20240219084947.php
+++ migrations/Version20240219084947.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20240215214158 extends AbstractMigration
+final class Version20240219084947 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,7 +20,10 @@ final class Version20240215214158 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE `order` ADD user_id INT NOT NULL');
+        $this->addSql('ALTER TABLE ligne_order ADD order_id INT DEFAULT NULL');
+        $this->addSql('ALTER TABLE ligne_order ADD CONSTRAINT FK_F68579A88D9F6D38 FOREIGN KEY (order_id) REFERENCES `order` (id)');
+        $this->addSql('CREATE INDEX IDX_F68579A88D9F6D38 ON ligne_order (order_id)');
+        $this->addSql('ALTER TABLE `order` ADD user_id INT NOT NULL, ADD ref VARCHAR(20) NOT NULL');
         $this->addSql('ALTER TABLE `order` ADD CONSTRAINT FK_F5299398A76ED395 FOREIGN KEY (user_id) REFERENCES user (id)');
         $this->addSql('CREATE INDEX IDX_F5299398A76ED395 ON `order` (user_id)');
     }
@@ -28,8 +31,11 @@ final class Version20240215214158 extends AbstractMigration
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE ligne_order DROP FOREIGN KEY FK_F68579A88D9F6D38');
+        $this->addSql('DROP INDEX IDX_F68579A88D9F6D38 ON ligne_order');
+        $this->addSql('ALTER TABLE ligne_order DROP order_id');
         $this->addSql('ALTER TABLE `order` DROP FOREIGN KEY FK_F5299398A76ED395');
         $this->addSql('DROP INDEX IDX_F5299398A76ED395 ON `order`');
-        $this->addSql('ALTER TABLE `order` DROP user_id');
+        $this->addSql('ALTER TABLE `order` DROP user_id, DROP ref');
     }
 }
