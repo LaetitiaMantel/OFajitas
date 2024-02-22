bd0e2b018cb3d1359080073acbe3451b03e9f2f6
annatariachvili <anna.tariachvili@icloud.com>
2024-02-22 21:06:56 +0100

    update

--- src/Controller/Front/UserController.php
+++ src/Controller/Front/UserController.php
@@ -17,9 +17,9 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
 
-#[Route('/', name:'front_user_')]
+#[Route('/', name: 'front_user_')]
 class UserController extends AbstractController
-{   
+{
     private $mailer;
 
     public function __construct(MailerInterface $mailer)
@@ -27,133 +27,135 @@ class UserController extends AbstractController
         $this->mailer = $mailer;
     }
 
-      // route pour afficher les informtions user
-      #[Route('/information', name: 'info')]
-      public function info() : Response 
-      {
-            // Récupérer l'utilisateur courant
-            $user = $this->getUser();
-   
-            // on envoie le resultat au twig associé
-            return $this->render('front/information/index.html.twig', [
-                'user'    => $user,
-            ]);
-      }
+    // route pour afficher les informtions user
+    #[Route('/information', name: 'info')]
+    public function info(): Response
+    {
+        // Récupérer l'utilisateur courant
+        $user = $this->getUser();
 
-      #[Route('/nouveau', name: 'new', methods: ['GET', 'POST'])]
-      public function new(Request $request, EntityManagerInterface $entityManager, UserPasswordHasherInterface $passwordHasher): Response
-      {
-          $user = new User();
-          $form = $this->createForm(UserTypeUser::class, $user);
-          $form->handleRequest($request);
+        // on envoie le resultat au twig associé
+        return $this->render('front/information/index.html.twig', [
+            'user'    => $user,
+        ]);
+    }
+
+    #[Route('/nouveau', name: 'new', methods: ['GET', 'POST'])]
+    public function new(Request $request, EntityManagerInterface $entityManager, UserPasswordHasherInterface $passwordHasher): Response
+    {
+        $user = new User();
+        $form = $this->createForm(UserTypeUser::class, $user);
+        $form->handleRequest($request);
         //TODO faire une confirmation de password
-          if ($form->isSubmitted() && $form->isValid()) {
-              $user->setPassword($passwordHasher->hashPassword($user, $user->getPassword()));
-              $user->setRoles(['ROLE_USER']);
-              $user->setIsBanned(false);
-              $entityManager->persist($user);
-              try {  
+        if ($form->isSubmitted() && $form->isValid()) {
+            $user->setPassword($passwordHasher->hashPassword($user, $user->getPassword()));
+            $user->setRoles(['ROLE_USER']);
+            $user->setIsBanned(false);
+            $entityManager->persist($user);
+            try {
                 $entityManager->flush();
-                 // Envoi de l'e-mail de confirmation
+                // Envoi de l'e-mail de confirmation
                 $email = (new Email())
-                ->from('jonathanaulnette53@gmail.com')
-                ->to($user->getEmail())
-                ->subject('Confirmation d\'inscription')
-                ->html($this->renderView(
-                    'email/confirmation.html.twig',
-                    ['user' => $user]
-                ));
-                
+                    ->from('jonathanaulnette53@gmail.com')
+                    ->to($user->getEmail())
+                    ->subject('Confirmation d\'inscription')
+                    ->html($this->renderView(
+                        'email/confirmation.html.twig',
+                        ['user' => $user]
+                    ));
+
                 $this->mailer->send($email);
                 $this->addFlash(
                     'success',
                     '<strong>' . $user->getFirstname() . '</strong> Vous avez recu un mail de confirmation.'
                 );
-                } catch (\Throwable $th) {
-                    $this->addFlash('warning','Cette adresse mail existe déjà');
-                }
-
-              
-  
-              return $this->redirectToRoute('front_main_home', [], Response::HTTP_SEE_OTHER);
-             //TODO voir pour que l utilisateur inscrit soit automatiquement connnecter
-         
-          }
-  
-          return $this->render('front/user/new.html.twig', [
-              'user' => $user,
-              'form' => $form,
-          ]);
-      }
-      
-      #[Route('/modifer/{email}', name: 'edit', methods: ['GET', 'POST'])]
-      public function edit(Request $request,string $email, EntityManagerInterface $entityManager, UserPasswordHasherInterface $passwordHasher): Response
-      {
-            $user = $entityManager->getRepository(User::class)->findOneBy(['email' => $email]);
-
-             // Vérifier si l'utilisateur existe
-            if (!$user) {
-                throw $this->createNotFoundException('Utilisateur non trouvé');
+            } catch (\Throwable $th) {
+                $this->addFlash('warning', 'Cette adresse mail existe déjà');
             }
 
-            $form = $this->createForm(UserTypeUser::class, $user);
-            $form->handleRequest($request);
-            //TODO faire une confirmation de password
-            if ($form->isSubmitted() && $form->isValid()) {
-                $newPassword = $form->get('password')->getData();
-                if ($newPassword) {
-                    $user->setPassword($passwordHasher->hashPassword($user, $newPassword));
-                }
-                $entityManager->flush();
-                $this->addFlash(
-                    'success',
-                    '<strong>' . $user->getFirstname() . '</strong> a été modifié dans votre base.'
-                );
-              
-                return $this->redirectToRoute('front_user_info', [], Response::HTTP_SEE_OTHER);
-            }
-          
-            return $this->render('front/user/edit.html.twig', [
-                'user' => $user,
-                'form' => $form,
-            ]);
-      }
 
-      #[Route('/supprimer/{email}', name: 'delete', methods: ['POST'])]
-      public function delete(Request $request, string $email, EntityManagerInterface $entityManager, 
-      TokenStorageInterface $tokenStorage): Response
-      {
-            $user = $entityManager->getRepository(User::class)->findOneBy(['email' => $email]);
-           
-            if ($this->isCsrfTokenValid('delete'.$user->getId(), $request->request->get('_token'))) {
-                $entityManager->remove($user);
-                $entityManager->flush();
+
+            return $this->redirectToRoute('front_main_home', [], Response::HTTP_SEE_OTHER);
+            //TODO voir pour que l utilisateur inscrit soit automatiquement connnecter
+
+        }
+
+        return $this->render('front/user/new.html.twig', [
+            'user' => $user,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/modifer/{email}', name: 'edit', methods: ['GET', 'POST'])]
+    public function edit(Request $request, string $email, EntityManagerInterface $entityManager, UserPasswordHasherInterface $passwordHasher): Response
+    {
+        $user = $entityManager->getRepository(User::class)->findOneBy(['email' => $email]);
+
+        // Vérifier si l'utilisateur existe
+        if (!$user) {
+            throw $this->createNotFoundException('Utilisateur non trouvé');
+        }
+
+        $form = $this->createForm(UserTypeUser::class, $user);
+        $form->handleRequest($request);
+        //TODO faire une confirmation de password
+        if ($form->isSubmitted() && $form->isValid()) {
+            $newPassword = $form->get('password')->getData();
+            if ($newPassword) {
+                $user->setPassword($passwordHasher->hashPassword($user, $newPassword));
             }
-            // Déconnexion de l'utilisateur après la suppression
-            $tokenStorage->setToken(null);
-            // invalide la session pour s'assurer que toutes les données de session sont effacées
-            $request->getSession()->invalidate(); 
-            
-             // Envoi de l'e-mail de confirmation
-             $email = (new Email())
-             ->from('jonathanaulnette53@gmail.com')
-             ->to($user->getEmail())
-             ->subject('Suppression de votre compte')
-             ->html($this->renderView(
-                 'email/suppression.html.twig',
-                 ['user' => $user]
-             ));
-             
-             $this->mailer->send($email);
-            // Envoyer un message flash
+            $entityManager->flush();
             $this->addFlash(
                 'success',
-                'Votre compte a été supprimé avec succès.'
+                '<strong>' . $user->getFirstname() . '</strong> a été modifié dans votre base.'
             );
 
-            return $this->redirectToRoute('front_main_home');
-          
-      }
+            return $this->redirectToRoute('front_user_info', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->render('front/user/edit.html.twig', [
+            'user' => $user,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/supprimer/{email}', name: 'delete', methods: ['POST'])]
+    public function delete(
+        Request $request,
+        string $email,
+        EntityManagerInterface $entityManager,
+        TokenStorageInterface $tokenStorage
+    ): Response {
+        $user = $entityManager->getRepository(User::class)->findOneBy(['email' => $email]);
+
+        if ($this->isCsrfTokenValid('delete' . $user->getId(), $request->request->get('_token'))) {
+            $entityManager->remove($user);
+            $entityManager->flush();
+        }
+        // Déconnexion de l'utilisateur après la suppression
+        $tokenStorage->setToken(null);
+        // invalide la session pour s'assurer que toutes les données de session sont effacées
+        $request->getSession()->invalidate();
+
+        // Envoi de l'e-mail de confirmation
+        $email = (new Email())
+            ->from('jonathanaulnette53@gmail.com')
+            ->to($user->getEmail())
+            ->subject('Suppression de votre compte')
+            ->html($this->renderView(
+                'email/suppression.html.twig',
+                ['user' => $user]
+            ));
+
+        $this->mailer->send($email);
+        // Envoyer un message flash
+        $this->addFlash(
+            'success',
+            'Votre compte a été supprimé avec succès.'
+        );
+
+        return $this->redirectToRoute('front_main_home');
+    }
 
     #[Route('/mes-achats', name: 'order_info')]
     public function orderInfo(EntityManagerInterface $entityManager): Response
@@ -179,19 +181,16 @@ class UserController extends AbstractController
     }
 
     #[Route('/connecter', name: 'connecter')]
-        public function connecter(): JsonResponse
-            {
-                // Récupérer l'utilisateur courant
-                $user = $this->getUser();
-                
-                if ($user) {
-                    
-                    return new JsonResponse(['success' => $user->User->getFirstname() .' utilisateur connécter.'], Response::HTTP_OK);
-                } else {
-                    // Le produit était déjà dans les favoris
-                    return new JsonResponse(['info' =>  'false'], Response::HTTP_OK);
-                }
-            }
+    public function connecter(): JsonResponse
+    {
+        // Récupérer l'utilisateur courant
+        $user = $this->getUser();
 
-
+        if ($user) {
+            return new JsonResponse(['success' => $user->getFirstname() . ' utilisateur connécter.'], Response::HTTP_OK);
+        } else {
+            // Le produit était déjà dans les favoris
+            return new JsonResponse(['info' =>  'false'], Response::HTTP_OK);
+        }
+    }
 }
