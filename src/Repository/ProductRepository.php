75f0b31c65033fd59edbd9e887aeaa744164ac73
Laetitia Mantel <laetitia.lecroart@gmail.com>
2024-10-08 14:53:16 +0200

    indexation ok

--- src/Repository/ProductRepository.php
+++ src/Repository/ProductRepository.php
@@ -20,70 +20,69 @@ class ProductRepository extends ServiceEntityRepository
     {
         parent::__construct($registry, Product::class);
     }
-    
+
 
     // Pour trouver les produits selon les marques : 
 
     public function findByBrand($slug)
-{
-    return $this->createQueryBuilder('p')
-        ->join('p.brand', 'b')
-        ->andWhere('b.slug = :slug')
-        ->setParameter('slug', $slug)
-        ->getQuery()
-        ->getResult();
-}
+    {
+        return $this->createQueryBuilder('p')
+            ->join('p.brand', 'b')
+            ->andWhere('b.slug = :slug')
+            ->setParameter('slug', $slug)
+            ->getQuery()
+            ->getResult();
+    }
 
-  // Pour trouver les produits selon les categories : 
+    // Pour trouver les produits selon les categories : 
 
-  public function findByCategory($slug)
-  {
-      return $this->createQueryBuilder('p')
-          ->join('p.category', 'b')
-          ->andWhere('b.slug = :slug')
-          ->setParameter('slug', $slug)
-          ->getQuery()
-          ->getResult();
-  }
+    public function findByCategory($slug)
+    {
+        return $this->createQueryBuilder('p')
+            ->join('p.category', 'b')
+            ->andWhere('b.slug = :slug')
+            ->setParameter('slug', $slug)
+            ->getQuery()
+            ->getResult();
+    }
 
-  // Pour trouver par recherche
-  public function findByResearch($search)
-  {
-            // on crée la requête sur l'objet Product ('c') 
-       $qb = $this->createQueryBuilder('p')
-            // on cherche les produit ou le parametre de recherche est present
+    // Pour trouver par recherche
+    public function findByResearch($search)
+    {
+        // on crée la requête sur l'objet Product ('p') 
+        $qb = $this->createQueryBuilder('p')
+            // on cherche les produits où le parametre de recherche est present
             ->where('p.name LIKE :param')
             // on fait une requete préparée (:param) pour éviter les injections SQL
             ->setParameter('param', '%' . $search . '%');
-            // on execute et on renvoie le tableau de résultats
-       return $qb->getQuery()->getResult();
-            
-  }
+        // on execute et on renvoie le tableau de résultats
+        return $qb->getQuery()->getResult();
+    }
 
 
 
-//    /**
-//     * @return Product[] Returns an array of Product objects
-//     */
-//    public function findByExampleField($value): array
-//    {
-//        return $this->createQueryBuilder('p')
-//            ->andWhere('p.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->orderBy('p.id', 'ASC')
-//            ->setMaxResults(10)
-//            ->getQuery()
-//            ->getResult()
-//        ;
-//    }
+    //    /**
+    //     * @return Product[] Returns an array of Product objects
+    //     */
+    //    public function findByExampleField($value): array
+    //    {
+    //        return $this->createQueryBuilder('p')
+    //            ->andWhere('p.exampleField = :val')
+    //            ->setParameter('val', $value)
+    //            ->orderBy('p.id', 'ASC')
+    //            ->setMaxResults(10)
+    //            ->getQuery()
+    //            ->getResult()
+    //        ;
+    //    }
 
-//    public function findOneBySomeField($value): ?Product
-//    {
-//        return $this->createQueryBuilder('p')
-//            ->andWhere('p.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->getQuery()
-//            ->getOneOrNullResult()
-//        ;
-//    }
+    //    public function findOneBySomeField($value): ?Product
+    //    {
+    //        return $this->createQueryBuilder('p')
+    //            ->andWhere('p.exampleField = :val')
+    //            ->setParameter('val', $value)
+    //            ->getQuery()
+    //            ->getOneOrNullResult()
+    //        ;
+    //    }
 }
